f75662d96f582d57d6a10bb8983aa3a1629339e6
Po-An, Lin <plin0863@uni.sydney.edu.au>
2026-04-07 03:24:00 +1000

    feat: add Docker support and update GitHub Actions for container deployment

--- src/main/java/com/acShop/config/RateLimitConfig.java
+++ src/main/java/com/acShop/config/RateLimitConfig.java
@@ -3,14 +3,22 @@ package com.acShop.config;
 import io.github.bucket4j.distributed.proxy.ProxyManager;
 import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
 import io.lettuce.core.RedisClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RateLimitConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
     @Bean
     public RedisClient redisClient(){
-        return  RedisClient.create("redis://localhost:6379");
+        return  RedisClient.create("redis://" + redisHost + ":" + redisPort);
     }
 
     @Bean
